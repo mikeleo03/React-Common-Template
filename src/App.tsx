adaa5d41238d0b5d02a4487cf6af500c5592f634
mikeleo03 <leonmichael463@gmail.com>
2024-10-18 23:26:53 +0700

    [Feat] Base components: Navbar and Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
-import './App.css'
+import { RouterProvider } from "react-router-dom";
+
+// styles
+import "./App.css";
+import router from "./routes";
+
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
-    <div className="bg-black text-white text-xl">
-      WELCOME TO THE APP
-    </div>
-  )
+    <>
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
-export default App
+export default App;
